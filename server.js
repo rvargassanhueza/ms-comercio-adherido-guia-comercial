a40f67ba33ea10717e47c6164422295bb4d89852
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2021-06-16 17:01:43 -0400

    cambio puerto front end

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ comercioAdheridoRouters.register(server);
 
 const cors = corsMiddleware({
   preflightMaxAge: 5,
-  origins: ['http://localhost:5001'],
+  origins: ['http://localhost:4200'],
   allowHeaders: ['Authorization'],
   allowMethods: ['*'],
   exposeHeaders: ['API-Token-Expiry']
